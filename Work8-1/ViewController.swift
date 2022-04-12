3e6eb381a4c0a936f05ac527af5b06a9af01149f
JIM90108 <jim90108@gmail.com>
2022-04-12 17:34:41 +0800

    commit message

--- Work8-1/ViewController.swift
+++ Work8-1/ViewController.swift
@@ -6,14 +6,160 @@
 //
 
 import UIKit
+import SpriteKit
+
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    
+    @IBOutlet weak var carImage: UIImageView!
+    @IBOutlet weak var carName: UILabel!
+    @IBOutlet weak var gender: UISegmentedControl!
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var birthDay: UIDatePicker!
+    @IBOutlet weak var gasolineSwitch: UISwitch!
+    @IBOutlet weak var dieselfuelSwitch: UISwitch!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var searchButton: UIButton!
+    
+    
+    
+    //存取
+    var carIdx = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        gasolineSwitch.isOn = false
+        dieselfuelSwitch.isOn = false
+        background()
         // Do any additional setup after loading the view.
     }
+    
+    
+    //重置
+    @IBAction func reset(_ sender: UIButton) {
+        //變回原始模擬器剛開始的模樣
+        carImage.image = UIImage(named: "background")
+        carName.text = "車型"
+        gasolineSwitch.isOn = false
+        dieselfuelSwitch.isOn = false
+        name.text = ""
+        gender.selectedSegmentIndex = 0
+    }
+    
+    //搜尋
+    @IBAction func search(_ sender: UIButton) {
+        SearchMyCar()
+       
+    }
+    
+    //點擊收鍵盤
+    @IBAction func keyBoardOver(_ sender: UITapGestureRecognizer) {
+            view.endEditing(true)
+        }
+    
+    
+    func SearchMyCar(){
+        carIdx += 1
+        //男生
+        if gender.selectedSegmentIndex == 0{
+            //每點擊一次搜尋都換切換圖片
+            if carIdx < AllCarMessage.count{
+                if gasolineSwitch.isOn == true && dieselfuelSwitch.isOn == true || gasolineSwitch.isOn == false && dieselfuelSwitch.isOn == false {
+                    carName.text = AllCarMessage[carIdx].carName
+                    carImage.image = UIImage(named: AllCarMessage[carIdx].carPic)
+                } else if gasolineSwitch.isOn == true && dieselfuelSwitch.isOn == false{
+                    if carIdx < allGasCarMessage.count{
+                        carName.text = allGasCarMessage[carIdx].carName
+                        carImage.image = UIImage(named: allGasCarMessage[carIdx].carPic)
+                    }else{
+                        carIdx = 0
+                        carName.text = allGasCarMessage[carIdx].carName
+                        carImage.image = UIImage(named: allGasCarMessage[carIdx].carPic)
+                    }
+                }else{
+                    if carIdx < allDieselCarMessage.count{
+                        carName.text = allDieselCarMessage[carIdx].carName
+                        carImage.image = UIImage(named: allDieselCarMessage[carIdx].carPic)
+                    }else{
+                        carIdx = 0
+                        carName.text = allDieselCarMessage[carIdx].carName
+                        carImage.image = UIImage(named: allDieselCarMessage[carIdx].carPic)
+                    }
+                    
+                }
+                
+            }else{
+                carIdx = 0
+                carName.text = AllCarMessage[carIdx].carName
+                carImage.image = UIImage(named: AllCarMessage[carIdx].carPic)
+                
+            }
+            
+        }else{
+            if carIdx < AllgirlsCarMessage.count{
+                if gasolineSwitch.isOn == true && dieselfuelSwitch.isOn == true || gasolineSwitch.isOn == false && dieselfuelSwitch.isOn == false {
+                    carName.text = AllgirlsCarMessage[carIdx].carName
+                    carImage.image = UIImage(named: AllgirlsCarMessage[carIdx].carPic)
+                }else if gasolineSwitch.isOn == true && dieselfuelSwitch.isOn == false {
+                    if carIdx < AllgirlsGasCar.count {
+                        carName.text = AllgirlsGasCar[carIdx].carName
+                        carImage.image = UIImage(named: AllgirlsGasCar[carIdx].carPic)
+                    }else{
+                        carIdx = 0
+                        carName.text = AllgirlsGasCar[carIdx].carName
+                        carImage.image = UIImage(named: AllgirlsGasCar[carIdx].carPic)
+                    }
+                }else{
+                    if carIdx < AllgirlsDieselCarMessage.count{
+                        carName.text = AllgirlsDieselCarMessage[carIdx].carName
+                        carImage.image = UIImage(named: AllgirlsDieselCarMessage[carIdx].carPic)
+                    }else{
+                        carIdx = 0
+                        carName.text = AllgirlsDieselCarMessage[carIdx].carName
+                        carImage.image = UIImage(named: AllgirlsDieselCarMessage[carIdx].carPic)
+                    }
+                }
+                
+            }else{
+                carIdx = 0
+                carName.text = AllgirlsCarMessage[carIdx].carName
+                carImage.image = UIImage(named: AllgirlsCarMessage[carIdx].carPic)
+            }
+            
+        }
+        
+    
 
+    }
+    
+    //漸層背景
+    func background(){
+        let skView = SKView(frame: self.view.bounds)
+        self.view.insertSubview(skView, at: 0)
+
+        let scene = SKScene(size: skView.frame.size)
+        scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        scene.backgroundColor = UIColor.init(red: 0/255, green: 0/255, blue: 0/255, alpha: 0)
+        skView.presentScene(scene)
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds//讓漸層的大小等於 controller view 的大小
+        gradientLayer.colors = [CGColor(srgbRed: 0, green: 1, blue: 1, alpha: 1),
+                                CGColor(srgbRed: 0, green: 0, blue: 1, alpha: 0.3)]
+        
+        skView.layer.addSublayer(gradientLayer )
+        
+        
+    }
 
 }
+            
+            
+            
+    
 
---
+++ Work8-1/carList.swift
@@ -0,0 +1,126 @@
+//
+//  carList.swift
+//  Work8-1
+//
+//  Created by 彭有駿 on 2022/4/12.
+//
+
+import Foundation
+
+
+struct gascar{
+    
+    var carName = ""
+    var carPic = ""
+    
+}
+var allGasCarMessage = [
+    
+    gascar(carName: "Ford Focus 5D", carPic: "Ford Focus 5D"),
+    gascar(carName: "Kia Picanto", carPic: "Kia Picanto"),
+    gascar(carName: "M-Benz C-Class Sedan", carPic: "M-Benz C-Class Sedan"),
+    gascar(carName: "Mazda CX-3", carPic: "Mazda CX-3"),
+    gascar(carName: "Mazda CX-5", carPic: "Mazda CX-5"),
+    gascar(carName: "Mini Hatch", carPic: "Mini Hatch"),
+    gascar(carName: "Nissan Juke", carPic: "Nissan Juke"),
+    gascar(carName: "Suzuki Swift", carPic: "Suzuki Swift"),
+    gascar(carName: "Toyota Corolla Cross", carPic: "Toyota Corolla Cross"),
+    gascar(carName: "Volkswagen Polo", carPic: "Volkswagen Polo")
+                     
+]
+
+
+struct dieselcar{
+    var carName = ""
+    var carPic = ""
+}
+var allDieselCarMessage = [
+
+dieselcar(carName: "BMW X5", carPic: "BMW X5"),
+dieselcar(carName: "Toyota Hilux", carPic: "Toyota Hilux"),
+dieselcar(carName: "Peugeot 3008", carPic: "Peugeot 3008"),
+dieselcar(carName: "Kia Sorento", carPic: "Kia Sorento"),
+dieselcar(carName: "M-Benz S-Class S350d", carPic: "M-Benz S-Class S350d")
+
+]
+
+
+
+
+
+
+struct girlscar {
+    var carName = ""
+    var carPic = ""
+}
+var AllgirlsCarMessage = [
+
+girlscar(carName: "Kia Picanto", carPic: "Kia Picanto"),
+girlscar(carName: "Mazda CX-3", carPic: "Mazda CX-3"),
+girlscar(carName: "Mini Hatch", carPic: "Mini Hatch"),
+girlscar(carName: "Nissan Juke", carPic: "Nissan Juke"),
+girlscar(carName: "Suzuki Swift", carPic: "Suzuki Swift"),
+girlscar(carName: "Volkswagen Polo", carPic: "Volkswagen Polo"),
+girlscar(carName: "M-Benz S-Class S350d", carPic: "M-Benz S-Class S350d"),
+girlscar(carName: "Citroen C4 Picasso Premium", carPic: "Citroen C4 Picasso Premium")
+
+]
+
+
+
+struct girlsdieselcar{
+    var carName = ""
+    var carPic = ""
+}
+var AllgirlsDieselCarMessage = [
+
+girlsdieselcar(carName: "M-Benz S-Class S350d", carPic: "M-Benz S-Class S350d"),
+girlsdieselcar(carName: "Citroen C4 Picasso Premium", carPic: "Citroen C4 Picasso Premium")
+
+]
+
+
+struct girlsgascar{
+    var carName = ""
+    var carPic = ""
+}
+var AllgirlsGasCar = [
+    girlsgascar(carName: "Kia Picanto", carPic: "Kia Picanto"),
+    girlsgascar(carName: "Mazda CX-3", carPic: "Mazda CX-3"),
+    girlsgascar(carName: "Mini Hatch", carPic: "Mini Hatch"),
+    girlsgascar(carName: "Nissan Juke", carPic: "Nissan Juke"),
+    girlsgascar(carName: "Suzuki Swift", carPic: "Suzuki Swift"),
+    girlsgascar(carName: "Volkswagen Polo", carPic: "Volkswagen Polo")
+]
+
+
+
+
+
+
+
+
+struct car{
+    var carName = ""
+    var carPic = ""
+}
+
+var AllCarMessage = [
+
+car(carName: "Ford Focus 5D", carPic: "Ford Focus 5D"),
+car(carName: "Kia Picanto", carPic: "Kia Picanto"),
+car(carName: "M-Benz C-Class Sedan", carPic: "M-Benz C-Class Sedan"),
+car(carName: "Mazda CX-3", carPic: "Mazda CX-3"),
+car(carName: "Mazda CX-5", carPic: "Mazda CX-5"),
+car(carName: "Mini Hatch", carPic: "Mini Hatch"),
+car(carName: "Nissan Juke", carPic: "Nissan Juke"),
+car(carName: "Suzuki Swift", carPic: "Suzuki Swift"),
+car(carName: "Toyota Corolla Cross", carPic: "Toyota Corolla Cross"),
+car(carName: "BMW X5", carPic: "BMW X5"),
+car(carName: "Toyota Hilux", carPic: "Toyota Hilux"),
+car(carName: "Peugeot 3008", carPic: "Peugeot 3008"),
+car(carName: "Kia Sorento", carPic: "Kia Sorento"),
+car(carName: "M-Benz S-Class S350d", carPic: "M-Benz S-Class S350d"),
+car(carName: "Citroen C4 Picasso Premium", carPic: "Citroen C4 Picasso Premium")
+
+]
